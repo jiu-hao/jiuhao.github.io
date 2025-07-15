1c93450a3954526a24364250f3de1ecf13327c6c
linyanshan <1294254846@qq.com>
2025-07-15 10:57:48 +0800

    feat:【VuePress】更改一些配置文件

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"Mine Blog\",\"description\":\"个人博客测试\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/jiuhao.github.io/\",\"lang\":\"zh-CN\",\"title\":\"Mine Blog\",\"description\":\"个人博客测试\",\"head\":[],\"locales\":{}}")
